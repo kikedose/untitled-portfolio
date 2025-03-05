f8c14b7220d5e20d6577b5b5aafeef87bebf4ffa
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-03-04 22:45:18 -0300

    updated api route fetching

--- src/xhr/utils.js
+++ src/xhr/utils.js
@@ -1,5 +1,7 @@
 export const fetchProjects = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api`, { cache: 'no-store' });
+  // const res = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api`, { cache: 'no-store' });
+  const res = await fetch('/api', { cache: 'force-cache' });
+
 
   if (!res.ok) {
     throw new Error('Failed to fetch projects');
