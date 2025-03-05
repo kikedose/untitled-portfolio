95213a4ed7f6b661a9cac8f07b31523d05858b5a
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-03-05 00:49:05 -0300

    gradient overlay

--- src/app/page.js
+++ src/app/page.js
@@ -6,26 +6,31 @@ export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 bg-zinc-950 px-8 pt-24 pb-8">
-        <section className="h-auto">
-          <div className="w-lg pb-16">
-            <h1 className="text-8xl font-bold">Designs that breathe.</h1>
+        <section className="h-auto flex flex-row radial-bg rounded-md">
+          <div className="w-lg py-16">
+            <h1 className="text-8xl font-bold">
+              Designs that{' '}
+              <span className="block rounded-md border bg-zinc-50 py-4 mt-4 pl-16 text-zinc-950">
+                breathe.
+              </span>
+            </h1>
+
+            <p className="max-w-[50ch] pt-8 font-medium text-zinc-50">
+              We&apos;re dedicated to crafting distinctive visual identities
+              that speak to the essence of each brand. We specialize in
+              designing logos, typography, and other essential elements that
+              help businesses stand out in a crowded market.
+            </p>
           </div>
         </section>
 
         <section className="h-auto rounded-lg bg-white">
           <div className="w-full p-8">
-            <p className="w-[50ch] font-bold text-zinc-950">
-              We&apos;re dedicated to crafting distinctive visual identities that
-              speak to the essence of each brand. We specialize in designing
-              logos, typography, and other essential elements that help
-              businesses stand out in a crowded market.
-            </p>
-
-            <ul className='pt-16'>
+            <ul className="">
               <li className="py-4">
                 <Link
                   href="/work"
-                  className="text-4xl font-bold text-zinc-950 hover:pl-8 transition-padding duration-200"
+                  className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
                 >
                   Check out our work {'->'}
                 </Link>
@@ -35,7 +40,7 @@ export default function Home() {
                 <a
                   href="mailto:edominguez.se@gmail.com"
                   rel="noopener noreferrer"
-                  className="text-4xl font-bold text-zinc-950 hover:pl-8 transition-padding duration-200"
+                  className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
                 >
                   Tell us about your project! {'->'}
                 </a>
