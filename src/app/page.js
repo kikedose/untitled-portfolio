b9563e64aacf2b4bb434da23c4513a64bc75e329
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-03-06 20:58:57 -0300

    wip: responsive homescreen

--- src/app/page.js
+++ src/app/page.js
@@ -4,51 +4,44 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 bg-zinc-950 px-8 pt-24 pb-8">
-        <section className="h-auto flex flex-row radial-bg rounded-md">
-          <div className="w-lg py-16">
-            <h1 className="text-8xl font-bold">
-              Designs that{' '}
-              <span className="block rounded-md border bg-zinc-50 py-4 mt-4 pl-16 text-zinc-950">
-                breathe.
-              </span>
-            </h1>
+    <main className="radial-bg flex w-full flex-col gap-8 bg-zinc-950 md:px-8 md:pt-24 pb-8 font-[family-name:var(--font-geist-sans)]">
+      <section className="px-8 flex h-auto w-full flex-row rounded-md">
+        <div className="md:w-lg py-16">
+          <h1 className="w-min text-6xl md:text-8xl font-bold">
+            Designs that breathe.
+          </h1>
 
-            <p className="max-w-[50ch] pt-8 font-medium text-zinc-50">
-              We&apos;re dedicated to crafting distinctive visual identities
-              that speak to the essence of each brand. We specialize in
-              designing logos, typography, and other essential elements that
-              help businesses stand out in a crowded market.
-            </p>
-          </div>
-        </section>
+          <p className="max-w-[50ch] pt-8 font-medium">
+            We&apos;re dedicated to crafting distinctive visual identities that
+            speak to the essence of each brand.
+          </p>
+        </div>
+      </section>
 
-        <section className="h-auto rounded-lg bg-white">
-          <div className="w-full p-8">
-            <ul className="">
-              <li className="py-4">
-                <Link
-                  href="/work"
-                  className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
-                >
-                  Check out our work {'->'}
-                </Link>
-              </li>
+      <section className="h-auto rounded-lg bg-white">
+        <div className="w-full p-8">
+          <ul className="">
+            <li className="py-4">
+              <Link
+                href="/work"
+                className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
+              >
+                Check out our work {'->'}
+              </Link>
+            </li>
 
-              <li className="py-4">
-                <a
-                  href="mailto:edominguez.se@gmail.com"
-                  rel="noopener noreferrer"
-                  className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
-                >
-                  Tell us about your project! {'->'}
-                </a>
-              </li>
-            </ul>
-          </div>
-        </section>
-      </main>
-    </div>
+            <li className="py-4">
+              <a
+                href="mailto:edominguez.se@gmail.com"
+                rel="noopener noreferrer"
+                className="transition-padding text-4xl font-bold text-zinc-950 duration-200 hover:pl-8"
+              >
+                Tell us about your project! {'->'}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </main>
   );
 }
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,19 +2,19 @@ import Link from 'next/link';
 
 export default function Navigation() {
   return (
-    <nav className="fixed z-50 min-w-screen backdrop-blur-md bg-zinc-950/50">
-      <div className="pl-6 flex items-center justify-between h-16">
+    <nav className="z-50 hidden min-w-screen bg-zinc-950/50 backdrop-blur-md md:fixed md:block">
+      <div className="flex h-16 items-center justify-between pl-6">
         <div className="flex items-baseline gap-4">
           <Link
             href="/"
-            className="px-3 py-2 rounded-md text-sm font-[family-name:var(--font-geist-mono)] text-white border border-transparent hover:border-white"
+            className="rounded-md border border-transparent px-3 py-2 font-[family-name:var(--font-geist-mono)] text-sm text-white hover:border-white"
           >
             UNTITLED DESIGN GRAPHIQUE_
           </Link>
 
           <Link
             href="/work"
-            className="px-3 py-2 rounded-md text-sm font-[family-name:var(--font-geist-mono)] text-white border border-transparent hover:border-white"
+            className="rounded-md border border-transparent px-3 py-2 font-[family-name:var(--font-geist-mono)] text-sm text-white hover:border-white"
           >
             OUR WORK
           </Link>
