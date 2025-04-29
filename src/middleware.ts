1b3b44e5e5f65ca1af2396c2c4509e50f6b34073
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-04-29 22:16:58 +0000

    fix(middleware): ignores favicon req

--- src/middleware.ts
+++ src/middleware.ts
@@ -40,7 +40,13 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    // Skip all internal paths (_next)
-    '/((?!_next).*)',
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 };
