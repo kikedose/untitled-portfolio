ea49f3b4c21dad75c6b42d1256da04a906621eee
Enrique Dominguez <53445849+kikedose@users.noreply.github.com>
2025-12-09 20:34:29 -0300

    added basic theme capabilities

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 
 const config = {
-  trailingComma: "es5",
+  trailingComma: 'es5',
   tabWidth: 2,
   semi: true,
   singleQuote: true,
   printWidth: 80,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
 
 export default config;
