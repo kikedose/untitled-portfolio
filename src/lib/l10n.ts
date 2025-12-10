891c3a4b941577b201173d6d35ab4ca54b1287f9
Enrique Domínguez <53445849+kikedose@users.noreply.github.com>
2025-12-10 11:03:07 -0300

    initial format fix

--- src/lib/l10n.ts
+++ src/lib/l10n.ts
@@ -2,15 +2,10 @@ import 'server-only';
 import type { Locale } from './i18n';
 
 const dictionaries = {
-    en: () =>
-      import('~/lib/dictionaries/en.json').then(
-        (module) => module.default
-      ),
-    es: () =>
-      import('~/lib/dictionaries/es.json').then(
-        (module) => module.default
-      ),
+  en: () =>
+    import('~/lib/dictionaries/en.json').then((module) => module.default),
+  es: () =>
+    import('~/lib/dictionaries/es.json').then((module) => module.default),
 };
 
-export const getDictionary = async (locale: Locale) =>
-  dictionaries[locale]();
+export const getDictionary = async (locale: Locale) => dictionaries[locale]();
